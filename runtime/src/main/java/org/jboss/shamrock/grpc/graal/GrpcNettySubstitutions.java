fd0b24ead176150076c023aee46eb2796e720a6e
Harald Pehl <harald.pehl@gmail.com>
2019-02-11 09:39:44 +0100

    Add substitutions for gRPC Netty code

---
+++ runtime/src/main/java/org/jboss/shamrock/grpc/graal/GrpcNettySubstitutions.java
@@ -0,0 +1,37 @@
+package org.jboss.shamrock.grpc.graal;
+
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.oracle.svm.core.annotate.Substitute;
+import com.oracle.svm.core.annotate.TargetClass;
+import io.netty.channel.ChannelHandlerContext;
+
+
+@TargetClass(className = "io.grpc.netty.ProtocolNegotiators")
+final class Target_io_grpc_netty_ProtocolNegotiators {
+
+    @Substitute
+    static void logSslEngineDetails(Level level, ChannelHandlerContext ctx, String msg, Throwable t) {
+        Logger log = Logger.getLogger("io.grpc.netty.ProtocolNegotiators");
+        if (log.isLoggable(level)) {
+            StringBuilder builder = new StringBuilder(msg).append("\nNo SSLEngine details available!");
+            log.log(level, builder.toString(), t);
+        }
+    }
+}
+
+
+@TargetClass(className = "com.google.protobuf.UnsafeUtil")
+final class Target_com_google_protobuf_UnsafeUtil {
+
+    @Substitute
+    private static sun.misc.Unsafe getUnsafe() {
+        return null;
+    }
+}
+
+
+class GrpcNettySubstitutions {
+}
